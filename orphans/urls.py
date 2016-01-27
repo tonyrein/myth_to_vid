d3d52879e07c8d3125e349863297f1d4dee1b070
Tony Rein <trein@cinci.rr.com>
2016-01-27 12:15:45 -0500

    Removed unused url patterns

--- orphans/urls.py
+++ orphans/urls.py
@@ -4,13 +4,8 @@ from orphans.views import OrphanDeleteView, OrphanDetailView, OrphanListView, Or
 urlpatterns = [
     url(r'^$', OrphanListView.as_view(), name='OrphanListView'),
     url(r'^(?P<page>\d+)/$', OrphanListView.as_view(), name='OrphanListViewWithPage'),
-    url(r'^(?P<sort>\w+)/$', OrphanListView.as_view(), name='OrphanListViewWithSort'),
-    url(r'^(?P<page>\d+)/(?P<sort>\w+)/$', OrphanListView.as_view(), name='OrphanListViewWithPageAndSort'),
     url(r'^update/(?P<pk>\d+)/$', OrphanUpdateView.as_view(), name='OrphanUpdateView'),
     url(r'^delete/(?P<pk>\d+)/$', OrphanDeleteView.as_view(), name='OrphanDeleteView'),
-    url(r'^delete/(?P<pk>\d+)/(?P<listpage>\d+)/$', OrphanDeleteView.as_view(), name='OrphanDeleteViewWithListPage'),
-    url(r'^delete/(?P<pk>\d+)/(?P<listsort>\w+)/$', OrphanDeleteView.as_view(), name='OrphanDeleteViewWithListSort'),
-    url(r'^delete/(?P<pk>\d+)/(?P<listpage>\d+)/(?P<listsort>\w+)/$', OrphanDeleteView.as_view(), name='OrphanDeleteViewWithListPageAndSort'),
     url(r'^(?P<pk>\d+)/$', OrphanDetailView.as_view(), name='OrphanDetailView'),
                
 ]
